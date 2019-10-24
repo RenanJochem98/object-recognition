241267e133eed39dc10bc29c8ceeaa4c93937a53
DEVDIMAS03 <renanjochem98@gmail.com>
2019-10-24 16:59:29 -0300

    Adicionando reconhecimento de olhos na imagem

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import cv2 as cv
 
 pathOpenCVClassifiers = "/home/renanj/.local/lib/python3.6/site-packages/cv2/data"
 face_classifier = cv.CascadeClassifier(pathOpenCVClassifiers+'/haarcascade_frontalface_default.xml')
+eye_classifier = cv.CascadeClassifier(pathOpenCVClassifiers+'/haarcascade_eye.xml')
 # print(type(face_classifier))
 print("Lendo imagem")
 # image = cv.imread('image.jpg')
@@ -18,6 +19,11 @@ faces = face_classifier.detectMultiScale(image_gray, 1.3, 5)
 print("Formando retangulo na imagem")
 for (x,y,w,h) in faces:
     cv.rectangle(image, (x,y), (x+w, y+h), (255,0,0), 2)
+    roi_gray = image_gray[y:y+h, x:x+w]
+    roi_color = image[y:y+h, x:x+w]
+    eyes = eye_classifier.detectMultiScale(roi_gray)
+    for (ex,ey,ew,eh) in eyes:
+        cv.rectangle(roi_color, (ex,ey), (ex+ew, ey+eh), (0,255,0), 2)
 
 cv.imshow('image', image)
 cv.waitKey(0)
