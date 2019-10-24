ba2e6ba5defa1e43b2779659d38cb3859c3f7efd
DEVDIMAS03 <renanjochem98@gmail.com>
2019-10-24 16:06:24 -0300

    Corrigindo local dos tipos de classificadores de rostos

--- main.py
+++ main.py
@@ -4,19 +4,21 @@
 import numpy as np
 import cv2 as cv
 
-face_classifier = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
+pathOpenCVClassifiers = "/home/renanj/.local/lib/python3.6/site-packages/cv2/data"
+face_classifier = cv.CascadeClassifier(pathOpenCVClassifiers+'/haarcascade_frontalface_default.xml')
 # print(type(face_classifier))
 print("Lendo imagem")
-image = cv.imread('image.jpg')
+# image = cv.imread('image.jpg')
+image = cv.imread('image-small.jpg')
 print("Convertendo imagem em cinza")
 image_gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
 
 print("Detectando Escalas a imagem em cinza")
 faces = face_classifier.detectMultiScale(image_gray, 1.3, 5)
-# print("Formando retangulo na imagem")
-# for (x,y,w,h) in faces:
-#     cv.rectangle(image, (x,y), (x+w, y+h), (255,0,0), 2)
-#
+print("Formando retangulo na imagem")
+for (x,y,w,h) in faces:
+    cv.rectangle(image, (x,y), (x+w, y+h), (255,0,0), 2)
+
 cv.imshow('image', image)
 cv.waitKey(0)
 cv.destroyAllWindows()
