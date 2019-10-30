575db5c3845acfcced17a269f09790f474408261
DEVDIMAS03 <renanjochem98@gmail.com>
2019-10-30 16:21:26 -0300

    Adicionando busca de imagens do google

---
+++ ImagesTool.py
@@ -0,0 +1,27 @@
+# https://google-images-download.readthedocs.io/en/latest/arguments.html ## Argumentos para downloads do Google
+from google_images_download import google_images_download
+# from zipfile import ZipFile
+class ImagesTool:
+
+    def __init__(self):
+        self.downloader = google_images_download.googleimagesdownload()
+        self.defaultImagesPaths = 'images'
+
+    def downloadImages(self, keywords , limit = 10):
+        arguments = {"keywords":keywords,"limit":limit, "output_directory": self.defaultImagesPaths, "save_source": 'tree'}
+        paths = self.downloader.download(arguments)
+        # self.zip()
+
+    # def zip(self):
+    #     # specifying the zip file name
+    #     file_name = "my_python_files.zip"
+    #
+    #     # opening the zip file in READ mode
+    #     with ZipFile(file_name, 'r') as zip:
+    #         # printing all the contents of the zip file
+    #         zip.printdir()
+    #
+    #         # extracting all the files
+    #         print('Extracting all the files now...')
+    #         zip.extractall()
+    #         print('Done!')
